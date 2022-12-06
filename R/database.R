08e3b339a7107dbed0ee5c206ac7fc4bb4b0fb2a
Ben Best <ben@ecoquants.com>
2022-12-06 07:52:32 -0800

    ∆ create_index(): + is_ pfx for geom & unique

--- R/database.R
+++ R/database.R
@@ -46,8 +46,8 @@ cc_db_connect <- function(path_pw = "~/.calcofi_db_pass.txt"){
 #' @param con database connection object from `DBI::dbConnect()`, e.g. from `cc_db_connect()`
 #' @param tbl table name as character
 #' @param flds character vector of fields in table used for index
-#' @param geom logical (default: FALSE) whether geometry field, so create GIST() index
-#' @param unique logical (default: FALSE) whether to impose a unique constraint, to prevent duplicates; default: FALSE
+#' @param is_geom logical (default: FALSE) whether geometry field, so create GIST() index
+#' @param is_unique logical (default: FALSE) whether to impose a unique constraint, to prevent duplicates; default: FALSE
 #' @param overwrite logical (default: FALSE) whether to overwrite existing index
 #' @param show logical (default: FALSE) whether to show SQL statement
 #' @param exec logical (default: TRUE) whether to execute SQL statement
@@ -61,14 +61,14 @@ cc_db_connect <- function(path_pw = "~/.calcofi_db_pass.txt"){
 #' @examples
 #' \dontrun{
 #' con <- cc_db_connect()
-#' create_index(con, "ctd_casts", "geom", geom=T)
-#' create_index(con, "ctd_casts", "cast_count", unique=T)
+#' create_index(con, "ctd_casts", "geom", is_geom=T)
+#' create_index(con, "ctd_casts", "cast_count", is_unique=T)
 #' }
-create_index <- function(con, tbl, flds, geom=F, unique=F, overwrite=F, show=F, exec=T){
-  # tbl = "taxa"; flds = c("tbl_orig", "aphia_id"); unique = T; geom=F
-  stopifnot(!(geom == T & length(flds) != 1))
+create_index <- function(con, tbl, flds, is_geom=F, is_unique=F, overwrite=F, show=F, exec=T){
+  # tbl = "taxa"; flds = c("tbl_orig", "aphia_id"); is_unique = T; is_geom=F
+  stopifnot(!(is_geom == T & length(flds) != 1))
   sfx <- ifelse(
-    geom,
+    is_geom,
     glue::glue(" USING GIST ({flds})"),
     glue::glue("({paste(flds, collapse=', ')})"))
   idx <- glue("{tbl}_{paste(flds, collapse='_')}_idx")
@@ -77,7 +77,7 @@ create_index <- function(con, tbl, flds, geom=F, unique=F, overwrite=F, show=F, 
     dbSendQuery(con, glue("DROP INDEX IF EXISTS {idx}"))
 
   sql <- glue::glue(
-    "CREATE {ifelse(unique, 'UNIQUE','')} INDEX IF NOT EXISTS {idx} ON {tbl}{sfx}")
+    "CREATE {ifelse(is_unique, 'UNIQUE','')} INDEX IF NOT EXISTS {idx} ON {tbl}{sfx}")
   if (show)
     message(sql)
   if (exec)
